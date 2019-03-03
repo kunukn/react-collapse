486f3d2a73f99484a1c87dcc39eea7380df2208d
Kunuk Nykjær <kunukn@gmail.com>
2019-03-03 10:16:25 +0100

    refactoring

--- rollup-plugins/sizes-plugin.js
+++ rollup-plugins/sizes-plugin.js
@@ -1,14 +1,8 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
 import gzip from 'gzip-size';
 
 export default function sizes(options) {
   return {
-    name: 'scripts/rollup/plugins/sizes-plugin',
+    name: 'rollup-plugins/sizes-plugin',
     ongenerate(bundle, obj) {
       const size = Buffer.byteLength(obj.code);
       const gzipSize = gzip.sync(obj.code);
