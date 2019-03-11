ccde577ea9498409d9b6e753ec42ebd863e76f28
Kunuk Nykjær <kunukn@gmail.com>
2019-03-11 12:52:38 +0100

    upd demo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -18,14 +18,11 @@ class App extends React.Component {
     let C4 = this.props.Collapse4 ? this.props.Collapse4 : C;
 
     return (
-      <div className="app">
-        <button
-          className={cx('app__toggle', { 'app__toggle--active': this.state.isOpen1 })}
-          onClick={() => this.toggle(1)}
-        >
-          <span className="app__toggle-text">toggle</span>
-          <div className="rotate90">
-            <svg className={cx('icon', { 'icon--expanded': this.state.isOpen1 })} viewBox="6 0 12 24">
+      <div class="app">
+        <button class={cx('app__toggle', { 'app__toggle--active': this.state.isOpen1 })} onClick={() => this.toggle(1)}>
+          <span class="app__toggle-text">toggle</span>
+          <div class="rotate90">
+            <svg class={cx('icon', { 'icon--expanded': this.state.isOpen1 })} viewBox="6 0 12 24">
               <polygon points="8 0 6 1.8 14.4 12 6 22.2 8 24 18 12" />
             </svg>
           </div>
@@ -35,51 +32,47 @@ class App extends React.Component {
           onChange={data => console.log('onChange', data)}
           elementType="article"
           transition="height 280ms ease-in-out"
-          className={'app__collapse app__collapse--gradient' + (this.state.isOpen1 ? ' app__collapse--active' : '')}
+          class={'app__collapse app__collapse--gradient' + (this.state.isOpen1 ? ' app__collapse--active' : '')}
         >
-          <div className="app__content">
+          <div class="app__content">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
             ea commodo consequat.
-            <img className="image" alt="random" src="https://source.unsplash.com/user/erondu/500x200" />
-            <button onClick={() => this.toggle(1)} className="app__button">
+            <div class="image-wrapper">
+              <img class="image-wrapper__img" alt="random" src="https://source.unsplash.com/user/erondu/500x200" />
+            </div>
+            <button onClick={() => this.toggle(1)} class="app__button">
               close
             </button>
           </div>
         </C>
 
-        <button
-          className={cx('app__toggle', { 'app__toggle--active': this.state.isOpen2 })}
-          onClick={() => this.toggle(2)}
-        >
+        <button class={cx('app__toggle', { 'app__toggle--active': this.state.isOpen2 })} onClick={() => this.toggle(2)}>
           toggle
         </button>
 
         <C2
           isOpen={this.state.isOpen2}
-          className={'app__collapse app__collapse--gradient ' + (this.state.isOpen2 ? 'app__collapse--active' : '')}
+          class={'app__collapse app__collapse--gradient ' + (this.state.isOpen2 ? 'app__collapse--active' : '')}
           transition="height 800ms cubic-bezier(0.4, 0, 0.2, 1)"
           aria-hidden={this.state.isOpen2 ? 'false' : 'true'}
           onChange={data => console.log('onChange', data)}
           elementType="article"
           render={collapseState => (
-            <div className="app__content">
+            <div class="app__content">
               <div>{collapseState}</div>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
               ea commodo consequat.
-              <img className="image" alt="random" src="https://source.unsplash.com/user/erondu/500x200" />
-              <button onClick={() => this.toggle(2)} className="app__button">
+              <img class="app__image" alt="random" src="https://source.unsplash.com/user/erondu/500x200" />
+              <button onClick={() => this.toggle(2)} class="app__button">
                 close
               </button>
             </div>
           )}
         />
 
-        <button
-          className={cx('app__toggle', { 'app__toggle--active': this.state.isOpen3 })}
-          onClick={() => this.toggle(3)}
-        >
+        <button class={cx('app__toggle', { 'app__toggle--active': this.state.isOpen3 })} onClick={() => this.toggle(3)}>
           toggle
         </button>
 
@@ -87,11 +80,11 @@ class App extends React.Component {
         <C3
           isOpen={this.state.isOpen3}
           collapseHeight="60px"
-          className={'app__collapse app__collapse--gradient ' + (this.state.isOpen3 ? 'app__collapse--active' : '')}
+          class={'app__collapse app__collapse--gradient ' + (this.state.isOpen3 ? 'app__collapse--active' : '')}
           onInit={state => this.setState({ spy3: state })}
           onChange={state => this.setState({ spy3: state })}
           render={collapseState => (
-            <div className="app__content">
+            <div class="app__content">
               <div>{collapseState}</div>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
@@ -100,18 +93,15 @@ class App extends React.Component {
           )}
         />
 
-        <button
-          className={cx('app__toggle', { 'app__toggle--active': this.state.isOpen4 })}
-          onClick={() => this.toggle(4)}
-        >
+        <button class={cx('app__toggle', { 'app__toggle--active': this.state.isOpen4 })} onClick={() => this.toggle(4)}>
           toggle
         </button>
 
         <C4
           isOpen={this.state.isOpen4}
-          className={'app__collapse app__collapse--gradient ' + (this.state.isOpen4 ? 'app__collapse--active' : '')}
+          class={'app__collapse app__collapse--gradient ' + (this.state.isOpen4 ? 'app__collapse--active' : '')}
           render={collapseState => (
-            <div className="app__content">
+            <div class="app__content">
               <div>{collapseState}</div>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
@@ -120,7 +110,7 @@ class App extends React.Component {
           )}
         />
 
-        <p className="app__text">
+        <p class="app__text">
           Some content below <a href="/">dummy link</a>
         </p>
       </div>
