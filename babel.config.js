7c5fbc2197a6a29e6b0cd21e5cbc097f9d4780df
Kunuk Nykjær <kunukn@gmail.com>
2019-03-11 15:50:47 +0100

    add es2015

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,9 @@ module.exports = {
       '@babel/preset-env',
       {
         modules: false,
+        // targets: {
+        //   node: '6.5', /* ES2015 */
+        // },
       },
     ],
     '@babel/preset-react',
