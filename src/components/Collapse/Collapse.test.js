4be99fc66b4d44ed9147b1a7594d5374c171f8be
Kunuk Nykjær <kunukn@gmail.com>
2019-06-23 18:53:22 +0200

    upd

--- src/components/Collapse/Collapse.test.js
+++ src/components/Collapse/Collapse.test.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { render, fireEvent } from '@testing-library/react';
+//import { render, fireEvent } from '@testing-library/react';
 import Collapse from './Collapse';
 
 const wrap = (props = {}) => shallow(<Collapse {...props} />);
+const doMount = (props = {}) => mount(<Collapse {...props} />);
 
-test('render collapse', () => {
+test('render children', () => {
   const wrapper = wrap({
-    children: 'test',
+    children: 'test'
   });
 
   expect(wrapper.contains('test')).toBe(true);
+});
+
+test('render mounted children', () => {
+  const mounted = doMount({
+    children: 'test'
+  });
+  expect(mounted.contains('test')).toBe(true);
+  expect(mounted).to.have.style('height', '0px');
+  expect(mounted).to.have.style('visibility', 'hidden');
 });
--- src/test/jestSetup.js
+++ src/test/jestSetup.js
@@ -1,5 +1,6 @@
 import Enzyme, { shallow, render, mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+const { JSDOM } = require('jsdom');
 
 console.log('setupEnzyme');
 
@@ -9,6 +10,27 @@ configure({ adapter: new Adapter() });
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
-global.requestAnimationFrame = function(callback) {
-  setTimeout(callback, 16);
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+window.Date = Date;
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target)
+  });
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js'
 };
+global.requestAnimationFrame = function(callback) {
+  return setTimeout(callback, 0);
+};
+global.cancelAnimationFrame = function(id) {
+  clearTimeout(id);
+};
+copyProps(window, global);
