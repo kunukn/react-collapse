80fbbf6f2e4c4baa0644388f02b6df54299f6f3a
Kunuk Nykjær <kunukn@gmail.com>
2019-06-25 09:15:20 +0200

    start using tests

--- src/components/Collapse/Collapse.test.js
+++ src/components/Collapse/Collapse.test.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Collapse from './Collapse.hooks';
+import { render, fireEvent, cleanup, waitForElement } from '@testing-library/react';
+import 'jest-dom/extend-expect';
 
-const wrap = (props = {}) => global.enzymeShallow(<Collapse {...props} />);
-const wrapMount = (props = {}) => global.enzymeMount(<Collapse {...props} />);
-const wrapRender = (props = {}) => global.enzymeRender(<Collapse {...props} />);
+afterEach(cleanup);
 
-test('render children', () => {
-  const wrapper = wrap({
-    children: 'test'
+describe('<Collapse />', () => {
+  it('should render without errors', () => {
+    let Child = () => 'Some content';
+    const { getByText, getByTestId, container, asFragment } = render(
+      <Collapse>
+        <Child />
+      </Collapse>
+    );
+
+    let el = getByText('Some content');
+    expect(el).toBeTruthy();
   });
-
-  expect(wrapper.contains('test')).toBe(true);
 });
