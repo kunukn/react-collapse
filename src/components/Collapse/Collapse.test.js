4b5ea22dfe30755de298d547361504004d3960ad
Kunuk Nykjær <kunukn@gmail.com>
2019-06-25 22:03:20 +0200

    upd test

--- src/components/Collapse/Collapse.test.js
+++ src/components/Collapse/Collapse.test.js
@@ -13,12 +13,30 @@ describe('<Collapse />', () => {
   it('should render child', () => {
     let text = 'Some content';
     let Child = () => text;
-    const { getByText, findByText, getByTestId, container, asFragment } = render(
+    const { getByText } = render(
       <Collapse>
         <Child />
       </Collapse>
     );
 
     expect(getByText(text)).toBeTruthy();
+  });
+
+  it('should render collapsed by default', () => {
+    const { container } = render(<Collapse />);
+
+    const el = container.firstChild;
+
+    expect(el.style.height).toBe('0px');
+    expect(el.style.visibility).toBe('hidden');
+  });
+
+  it('should render default classNames', () => {
+    const { container } = render(<Collapse />);
+
+    const el = container.firstChild;
+
+    expect(el.className.indexOf('collapse-css-transition') >= 0).toBe(true);
+    expect(el.className.indexOf(' -c-is--collapsed') >= 0).toBe(true);
   });
 });
