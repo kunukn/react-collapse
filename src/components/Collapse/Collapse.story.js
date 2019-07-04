944a31ec984a893edfc97008fcc604965483f486
Kunuk Nykjær <kunukn@gmail.com>
2019-07-04 21:19:11 +0200

    upd npm modules

--- src/components/Collapse/Collapse.story.js
+++ src/components/Collapse/Collapse.story.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import Collapse from './Collapse';
-import { storiesOf } from '@storybook/react';
-import { withNotes, withMarkdownNotes } from '@storybook/addon-notes';
-import { action } from '@storybook/addon-actions';
-//import { linkTo } from '@storybook/addon-links';
-//import { backgrounds } from '~/stories/addon-backgrounds';
-import Component from '@reach/component-component';
-import 'components/Base/base.scss';
-import '~/stories/storybook.scss';
+import React from "react";
+import Collapse from "./Collapse.hooks";
+import { storiesOf } from "@storybook/react";
+import Component from "@reach/component-component";
+import "components/Base/base.scss";
+import "~/stories/storybook.scss";
 
 export default function CollapseStory() {
-
-  storiesOf('Collapses', module)
-    //.addDecorator(backgrounds)
-
-    .add('default', () => (
+  storiesOf("Collapses", module)
+    .add("default", () => (
       <Component initialState={{ isOpen: false }}>
         {({ state, setState }) => (
           <div className="box">
-            <button className="btn" onClick={() => setState({ isOpen: !state.isOpen })}>
+            <button
+              className="btn"
+              onClick={() => setState({ isOpen: !state.isOpen })}
+            >
               toggle
             </button>
             <Collapse isOpen={state.isOpen}>
               <p className="text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
               </p>
             </Collapse>
             <p className="text">below content</p>
@@ -34,31 +31,39 @@ export default function CollapseStory() {
       </Component>
     ))
 
-    .add('multiple', () => (
+    .add("multiple", () => (
       <Component initialState={{ isOpen1: false, isOpen2: false }}>
         {({ state, setState }) => (
           <>
             <div className="box">
-              <button className="btn" onClick={() => setState({ isOpen1: !state.isOpen1 })}>
+              <button
+                className="btn"
+                onClick={() => setState({ isOpen1: !state.isOpen1 })}
+              >
                 toggle
               </button>
               <Collapse isOpen={state.isOpen1}>
                 <p className="text">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
               </Collapse>
             </div>
             <div className="box">
-              <button className="btn" onClick={() => setState({ isOpen2: !state.isOpen2 })}>
+              <button
+                className="btn"
+                onClick={() => setState({ isOpen2: !state.isOpen2 })}
+              >
                 toggle
               </button>
               <Collapse isOpen={state.isOpen2}>
                 <p className="text">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
               </Collapse>
             </div>
