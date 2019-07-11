964f0260ad842cd4909126deb16d9be256f330ec
Kunuk Nykjær <kunukn@gmail.com>
2019-07-11 16:29:39 +0200

    upd

--- src/development-entry.js
+++ src/development-entry.js
@@ -1,16 +1,18 @@
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
-import 'components/Base/base.scss';
-import 'components/Collapse/collapse.css';
+import "components/Base/base.scss";
+import "components/Collapse/collapse.css";
 
-import App from 'components/App/App';
-import Collapse from 'components/Collapse/Collapse';
-import CollapseHooks from 'components/Collapse/Collapse.hooks';
+import App from "components/App/App";
+import Collapse from "components/Collapse/Collapse";
+import CollapseHooks from "components/Collapse/Collapse.hooks";
 
 ReactDOM.render(
   <StrictMode>
-    <App Collapse={CollapseHooks} />
+    <>
+      <App Collapse={CollapseHooks} />
+    </>
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
