bea1c8908bb1dc9fbdf5b0576340397da8a8a2e7
Kunuk Nykjær <knyk@novonordisk.com>
2024-04-06 16:17:29 +0200

    use React 18 mount api

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import './index.css'
 
 import App from './App.tsx'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
