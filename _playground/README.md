39744ee821be21b0107448adf795f89d692b701b
Kunuk Nykjær <knyk@novonordisk.com>
2024-04-07 22:01:22 +0200

    make playground work

--- _playground/README.md
+++ _playground/README.md
@@ -1,3 +1,3 @@
  ```bash
- npx browser-sync . -w
+ npx browser-sync . -w -o
  ```
--- _playground/main.js
+++ _playground/main.js
@@ -12,5 +12,23 @@ console.log(ReactDOM);
 
 const e = React.createElement;
 
+function MyComponent() {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const onToggle = () => setIsOpen((s) => !s)
+
+  return (
+    <div className="my-component">
+      <button onClick={onToggle}> Toggle </button>
+      <Collapse
+        isOpen={isOpen}
+        transition="height 300ms cubic-bezier(0.4, 0, 0.2, 1)"
+      >
+        <p> Hello world </p>
+        <p> How are you? </p>
+      </Collapse>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(e('div', null, 'Hello World'));
+root.render(<MyComponent />);
