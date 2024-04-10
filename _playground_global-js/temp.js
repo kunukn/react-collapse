17e68ece27cbb78b13304bc90b331035246c6ab2
Kunuk Nykjær <knyk@novonordisk.com>
2024-04-10 15:48:41 +0200

    refactorings

---
+++ _playground_global-js/temp.js
@@ -0,0 +1,14 @@
+;((R, g) => {
+    let polyfill = R ? R.createElement : () => {}
+    g.ReactJsxRuntime = {
+      /*
+        Polyfill the missing UMD required from the Collapse library.
+        Some other libraries seems to use jsxRuntime. I used ReactJsxRuntime for my library.
+      */
+  
+      jsx: polyfill,
+      jsxs: polyfill,
+      jsxDEV: polyfill,
+      jsxsDEV: polyfill,
+    }
+  })(React, globalThis)
--- public/ReactJsxRuntime.js
+++ public/ReactJsxRuntime.js
@@ -1,14 +1,14 @@
-{
-  const polyfill = React ? React.createElement : () => {} // React must be loaded and added to globalThis.React
-  globalThis.ReactJsxRuntime = {
+;((R, g) => {
+  let polyfill = R ? R.createElement : () => {}
+  g.ReactJsxRuntime = {
     /*
       Polyfill the missing UMD required from the Collapse library.
       Some other libraries seems to use jsxRuntime. I used ReactJsxRuntime for my library.
     */
-   
+
     jsx: polyfill,
     jsxs: polyfill,
     jsxDEV: polyfill,
     jsxsDEV: polyfill,
   }
-}
+})(React, globalThis)
